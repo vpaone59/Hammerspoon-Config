6538e2cd1e6667f19135e84c532bcff9759c661e
vinny <paone@rowan.edu>
2023-07-19 10:50:59 -0400

    Revised audio config for simplicity

--- audio.lua
+++ audio.lua
@@ -1,16 +1,14 @@
--- when CX True Wireless Earbuds are connected
+-- Detect when the current audio device is changed
+-- Show current audio device nad volume level
 function check_headphone_connectivity()
     curr_output = hs.audiodevice.defaultOutputDevice()
-    hs.alert.show(curr_output:volume() .. " - " .. curr_output:name())
+    hs.alert.show(math.floor(curr_output:volume() + 0.5) .. " - " .. curr_output:name())
 
-    local val = hs.execute("/usr/local/bin/blueutil --is-connected CX Plus True Wireless")
-    if string.sub(val, 1, 1) == "1" then
-        hs.alert.show("Bluetooth Connected to CX")
-    else
-        hs.alert.show("Bluetooth Disconnected from CX")
-        print("show")
-    end
 end
 
 hs.audiodevice.watcher.setCallback(check_headphone_connectivity)
 hs.audiodevice.watcher.start()
+
+if hs.audiodevice.watcher.isRunning() then
+    check_headphone_connectivity()
+end
